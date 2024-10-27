fd5852077734da24fe078fe9856fd492bda16a50
zero-sam <ups12b19@protonmail.com>
2024-10-27 05:01:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ echo "Updating package lists..."
 sudo apt update
 
 echo "Installing Tor and Python3..."
-sudo apt install -y tor python3 python3-pip
+sudo apt install -y tor python3 python3-pip --break-system-packages
 
 echo "Installing required Python libraries..."
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt --break-system-packages
 
 echo "Configuring Tor to allow control port access..."
-sudo bash -c 'echo -e "\nControlPort 9051\nCookieAuthentication 1" >> /etc/tor/torrc'
+sudo bash -c 'echo -e "\nControlPort 9051\nCookieAuthentication 1\nCookieAuthFile /run/tor/control.authcookie" >> /etc/tor/torrc'
 
 echo "Restarting Tor service..."
-sudo service tor restart
+sudo service tor restart 
 
 echo "Installation complete!"
